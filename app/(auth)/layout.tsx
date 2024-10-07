123f7fd4fba695e2103ae224da9ae8b19fb017c7
PARAS SAXENA <parassaxenaofficial@gmail.com>
2024-10-08 04:20:58 +0530

    Update layout.tsx for Clerk Auth at Top-Center area
    Clerk Auth aligns perfectly in the Top-Center area

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="mt-11 pt-11 border-t-8 border-transparent h-full flex items-center justify-center">{children}</div>
   );
 };
 
